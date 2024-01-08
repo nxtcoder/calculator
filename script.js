30271662fa2730f54bcc6fa834772e9a755a3542
Zaid <the1zaid2ahmad3@gmail.com>
2024-01-08 14:11:58 +0530

    css done

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 
 function operate (operator, num1, num2) {
-    if(operator = "add"){return add(num1, num2)}
-    else if(operator = "subtract"){return subtract(num1, num2)}
-    else if(operator = "multiply"){return multiply(num1, num2)}
-    else if(operator = "divide"){return divide(num1, num2)}
+    if(operator == "add"){return add(num1, num2)}
+    else if(operator == "subtract"){return subtract(num1, num2)}
+    else if(operator == "multiply"){return multiply(num1, num2)}
+    else if(operator == "divide"){return divide(num1, num2)}
     else{console.log("Syntax Error")}
 };
-
