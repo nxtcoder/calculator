5e526323e1048e8c91cf7999a0aa6e13604b9139
Zaid <the1zaid2ahmad3@gmail.com>
2024-01-09 20:43:24 +0530

    semi done js

--- script.js
+++ script.js
@@ -1,16 +1,69 @@
-let num1;
-let num2;
-let operator;
+let clear = document.querySelector(".clear")
+let backspace = document.querySelector(".backspace")
+let digits = document.querySelectorAll(".digit")
+let operators = document.querySelectorAll(".operator")
+let decimal = document.querySelector(".decimal")
+let equal = document.querySelector(".equal")
+let historyScreen = document.querySelector(".history")
+let currentScreen = document.querySelector(".current")
 
-const add = (a, b) => a + b
-const subtract = (a, b) => a - b
-const multiply = (a, b) => a * b
-const divide = (a, b) => a / b
+let operator,historyValue,currentValue = "";
 
-function operate (operator, num1, num2) {
-    if(operator == "add"){return add(num1, num2)}
-    else if(operator == "subtract"){return subtract(num1, num2)}
-    else if(operator == "multiply"){return multiply(num1, num2)}
-    else if(operator == "divide"){return divide(num1, num2)}
-    else{console.log("Syntax Error")}
-};
+digits.forEach((digit) => digit.addEventListener("click", function(e){
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue
+}))
+
+operators.forEach((op) => op.addEventListener("click", function(e){
+    handleOperator(e.target.textContent)
+    historyScreen.textContent = historyValue + " " + operator
+    currentScreen.textContent = currentValue
+}))
+
+decimal.addEventListener("click", function(){
+    addDecimal()
+})
+
+//need fix
+clear.addEventListener("click", function(){
+    historyScreen = ""
+    currentScreen = ""
+    operator = ""
+    historyScreen.textContent = ""
+    currentScreen.textContent = ""
+    console.log("clear")
+})
+
+equal.addEventListener("click", function(){
+    operate()
+    historyScreen.textContent = ""
+    currentScreen.textContent = historyValue
+})
+
+function operate(){
+    historyValue = Number(historyValue)
+    currentValue = Number(currentValue)
+
+    if(operator === "+"){historyValue += currentValue}
+    else if(operator === "-"){historyValue -= currentValue}
+    else if(operator === "*"){historyValue *= currentValue}
+    else if(operator === "/"){historyValue /= currentValue}
+    else {console.log("operation error")}
+    console.log(historyValue)
+}
+
+function handleNumber(number){
+    currentValue += number
+}
+
+function handleOperator(op){
+    operator = op
+    historyValue = currentValue
+    currentValue = ''
+}
+
+function addDecimal(){
+    if (!currentValue.includes(".")){
+        currentValue += "."
+    }
+}
